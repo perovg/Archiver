f0b27d3495d88e4aa983cbf6fe505a62ac90af58
Grigory Perov <gregory.perov@gmail.com>
2024-05-08 16:32:02 +0300

    Corrections for the review

--- src/caesar.py
+++ src/caesar.py
@@ -1,54 +1,62 @@
 import math
 
 
+"""----------------WORK WITH FILES----------------"""
+
+
+def read_file_contents(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+def write_file_contents(filename, contents, mode='w'):
+    with open(filename, mode, encoding='utf-8') as file:
+        file.write(contents)
+
+
 class Caesar:
-    # --------------------ENCODING--------------------
+    """--------------------ENCODING--------------------"""
+    @staticmethod
+    def encode_text(text, shift=3):
+        return ''.join(chr((ord(char) + shift) % 1114112) for char in text)
 
     @staticmethod
     def encoding(encode_file):
-        if encode_file[-4:] != ".txt":
-            exit(print("Error: Invalid file name"))
-        file_from = open(encode_file, encoding='utf-8')
-        file_to_name = list(str(encode_file).split("."))[0] + "_caesar.txt"
-        read_file = file_from.read()
-        file_from.close()
-        open(file_to_name, 'wb').close()
-        file_to = open(file_to_name, 'ab')
+        with open(encode_file, encoding='utf-8') as file_from:
+            file_to_name = f"{encode_file.split('.')[0]}_caesar.txt"
+            read_file = file_from.read()
+            file_from.close()
+            file_to = open(file_to_name, 'ab')
 
-        encoded_string = ""
-        file_length = math.ceil(len(read_file) / 10000)
-        for i in range(file_length):
-            if i < file_length - 1:
-                block = read_file[i * 10000: (i + 1) * 10000]
-            else:
-                block = read_file[i * 10000:]
-            for j in range(len(block)):
-                encoded_string += chr((ord(block[j]) + 3) % 1114112)
-            file_to.write(encoded_string.encode(encoding='utf-8'))
-            encoded_string = ""
-        file_to.close()
+            file_length = math.ceil(len(read_file) / 10000)
+            for i in range(file_length):
+                if i < file_length - 1:
+                    block = read_file[i * 10000: (i + 1) * 10000]
+                else:
+                    block = read_file[i * 10000:]
+                encoded_string = Caesar.encode_text(block)
+                file_to.write(encoded_string.encode(encoding='utf-8'))
+            file_to.close()
 
-    # --------------------DECODING--------------------
+    """--------------------DECODING--------------------"""
+
+    @staticmethod
+    def decode_text(text, shift=3):
+        return ''.join(chr((ord(char) - shift) % 1114112) for char in text)
 
     @staticmethod
     def decoding(decode_file):
-        if decode_file[-11:] != "_caesar.txt":
-            exit(print("Error: Invalid file name"))
-        file_from = open(decode_file, 'rb').read()
-        read_file = file_from.decode('utf-8')
-        file_to_name = list(str(decode_file).split("."))[0].split("_")[0] + ".txt"
-        open(file_to_name, 'w', encoding='utf-8').close()
-        file_to = open(file_to_name, 'a', encoding='utf-8')
+        with open(decode_file, 'rb').read() as file_from:
+            read_file = file_from.decode('utf-8')
+            file_to_name = f"{decode_file.split('.')[0]}.txt"
+            file_to = open(file_to_name, 'a', encoding='utf-8')
 
-        decode_string = ""
-        file_length = math.ceil(len(read_file) / 10000)
-        for i in range(file_length):
-            if i < file_length - 1:
-                block = read_file[i * 10000: (i + 1) * 10000]
-            else:
-                block = read_file[i * 10000:]
-            for j in range(len(block)):
-                decode_string += chr((ord(block[j]) + 1114109) % 1114112)
-            file_to.write(decode_string)
-            decode_string = ""
-        file_to.close()
+            file_length = math.ceil(len(read_file) / 10000)
+            for i in range(file_length):
+                if i < file_length - 1:
+                    block = read_file[i * 10000: (i + 1) * 10000]
+                else:
+                    block = read_file[i * 10000:]
+                decode_string = Caesar.decode_text(block)
+                file_to.write(decode_string)
+            file_to.close()
--- src/shennon.py
+++ src/shennon.py
@@ -1,84 +1,87 @@
-import math
-from math import copysign, fabs, floor, isfinite, modf
+from math import copysign, fabs, floor, isfinite, modf, ceil, log
 
 
 class Shennon:
-    # --------------------ENCODING--------------------
+    """--------------------ENCODING--------------------"""
+
+    """ Main encoding method """
     @staticmethod
-    def encoding(encode_file):  # Main encoding method 
-        if encode_file[-4:] != ".txt":
-            exit(print("Error: Invalid file name"))
-        file_from = open(encode_file, encoding='utf-8')
-        read_file = file_from.read()
+    def encoding(encode_file):
+        with open(encode_file, encoding='utf-8') as file_from:
+            read_file = file_from.read()
 
-        keys = Shennon.shennon(read_file)
-        bin_code = Shennon.encode(read_file, keys)
+            keys = Shennon.shennon(read_file)
+            bin_code = Shennon.encode(read_file, keys)
 
-        encoded_file = (str(Shennon.compress(bin_code)[1]) + '$').encode('utf-8')
-        keys = Shennon.write_keys(keys).encode('utf-8')
-        last_char = Shennon.compress(bin_code)[0].encode('utf-8')
-        less = (keys + encoded_file + last_char)
+            encoded_file = (str(Shennon.compress(bin_code)[1]) + '$').encode('utf-8')
+            keys = Shennon.write_keys(keys).encode('utf-8')
+            last_char = Shennon.compress(bin_code)[0].encode('utf-8')
+            less = (keys + encoded_file + last_char)
 
-        file_to_name = list(str(encode_file).split("."))[0] + ".prar"
-        file_to = open(file_to_name, 'wb')
-        file_to.write(less)
-        file_to.close()
+            file_to_name = f"{encode_file.split('.')[0]}.prar"
+            file_to = open(file_to_name, 'wb')
+            file_to.write(less)
+            file_to.close()
 
+    """ Translation the file to encrypted form """
     @staticmethod
-    def encode(string, key):  # Translation the file to encrypted form
-        binn = ''
-        j = ''
-        for i in string:
-            j += i
-            if j in key:
-                binn += key[j]
-                j = ''
-        return binn
+    def encode(string, key):
+        binary_string = ''
+        substring = ''
+        for sym in string:
+            substring += sym
+            if substring in key:
+                binary_string += key[substring]
+                substring = ''
+        return binary_string
 
+    """ Converting fractional numbers to binary number system """
     @staticmethod
-    def bi(f):  # Converting fractional numbers to binary number system
-        if not isfinite(f):
-            return repr(f)  # inf nan
+    def bi(fractional_number):
+        if not isfinite(fractional_number):
+            return repr(fractional_number)  # inf nan
 
-        sign = '-' * (copysign(1.0, f) < 0)
-        frac, fint = modf(fabs(f))  # split on fractional, integer parts
-        n, d = frac.as_integer_ratio()  # frac = numerator / denominator
-        assert d & (d - 1) == 0  # power of two
-        return f'{sign}{floor(fint):b}.{n:0{d.bit_length() - 1}b}'
+        sign = '-' * (copysign(1.0, fractional_number) < 0)
+        frac, fint = modf(fabs(fractional_number))  # split on fractional, integer parts
+        numerator, denominator = frac.as_integer_ratio()  # frac = numerator / denominator
+        assert denominator & (denominator - 1) == 0  # power of two
+        return f'{sign}{floor(fint):b}.{numerator:0{denominator.bit_length() - 1}b}'
 
+    """ Determining encryption keys """
     @staticmethod
-    def shennon(string):  # Determining encryption keys
+    def shennon(string):
         statistics = {}
 
-        for i in string:
-            statistics[i] = statistics.get(i, 0) + 1
-        for i in statistics:
-            statistics[i] = statistics[i] / len(string)
+        for sym in string:
+            statistics[sym] = statistics.get(sym, 0) + 1
+        for sym in statistics:
+            statistics[sym] = statistics[sym] / len(string)
         statistics = dict(sorted(statistics.items(), key=lambda x: x[1], reverse=True))
 
         auxiliary_dict = {}
         step = []
-        for i in range(len(list(statistics.values()))):
-            step.append(sum(list(statistics.values())[:i]))
-            step[i] = float(step[i])
+        for sym in range(len(list(statistics.values()))):
+            step.append(sum(list(statistics.values())[:sym]))
+            step[sym] = float(step[sym])
 
-        for i in statistics:
-            auxiliary_dict[i] = math.ceil(-(math.log(statistics[i], 2)))  # powers of two
+        for sym in statistics:
+            auxiliary_dict[sym] = ceil(-(log(statistics[sym], 2)))  # powers of two
 
-        for i in range(len(step)):
-            step[i] = str(Shennon.bi(step[i]))[2:]
+        for sym in range(len(step)):
+            step[sym] = str(Shennon.bi(step[sym]))[2:]
         sim = dict(zip(statistics.keys(), step))
 
         keys_dict = {}
-        for i in sim:
-            if len(sim[i]) < auxiliary_dict[i]:
-                while len(sim[i]) < auxiliary_dict[i]:
-                    sim[i] += '0'
-            keys_dict[i] = sim[i][:auxiliary_dict[i]]
+        for sym in sim:
+            if len(sim[sym]) < auxiliary_dict[sym]:
+                while len(sim[sym]) < auxiliary_dict[sym]:
+                    sim[sym] += '0'
+            keys_dict[sym] = sim[sym][:auxiliary_dict[sym]]
         return keys_dict
 
+    """ File compression """
     @staticmethod
-    def compress(string):  # File compression
+    def compress(string):
         cnt = 0
         while len(string) % 8 != 0:
             string += '0'
@@ -90,69 +93,72 @@ class Shennon:
             text += sym
         return text, cnt
 
+    """ Preparing encryption keys for recording """
     @staticmethod
-    def write_keys(keys_dictionary):  # Preparing encryption keys for recording
+    def write_keys(keys_dictionary):
         keys_string = ''
-        for i in keys_dictionary:
-            keys_string += i + keys_dictionary[i] + '||'
+        for key in keys_dictionary:
+            keys_string += key + keys_dictionary[key] + '||'
         keys_string += '$'
         return keys_string
 
-    # --------------------DECODING--------------------
+    """-------------------DECODING--------------------"""
 
+    """ Main decoding method """
     @staticmethod
-    def decoding(decode_file):  # Main decoding method
-        if decode_file[-5:] != ".prar":
-            exit(print("Error: Invalid file name"))
-        file_from = open(decode_file, 'rb').read()
-        read_file = file_from.decode('utf-8')
+    def decoding(decode_file):
+        with open(decode_file, 'rb').read() as file_from:
+            read_file = file_from.decode('utf-8')
 
-        border_1_idx = read_file.find('$')
-        keys = dict(Shennon.search_keys(read_file[:border_1_idx + 1], '$', '||'))
-        border_2_idx = read_file.find('$', border_1_idx + 1)
-        extra_char = int(read_file[border_1_idx + 1:border_2_idx])
-        encoded_file = read_file[border_2_idx + 1:]
-        decoded_file = Shennon.expand(encoded_file, extra_char)
+            border_1_idx = read_file.find('$')
+            keys = dict(Shennon.search_keys(read_file[:border_1_idx + 1], '$', '||'))
+            border_2_idx = read_file.find('$', border_1_idx + 1)
+            extra_char = int(read_file[border_1_idx + 1:border_2_idx])
+            encoded_file = read_file[border_2_idx + 1:]
+            decoded_file = Shennon.expand(encoded_file, extra_char)
 
-        file_to_name = list(str(decode_file).split("."))[0] + ".txt"
-        file_to = open(file_to_name, 'w', encoding='utf-8')
-        file_to.write(Shennon.decode(decoded_file, keys))
-        file_to.close()
+            file_to_name = f"{decode_file.split('.')[0]}.txt"
+            file_to = open(file_to_name, 'w', encoding='utf-8')
+            file_to.write(Shennon.decode(decoded_file, keys))
+            file_to.close()
 
+    """ Translated file from encrypted form """
     @staticmethod
-    def decode(string, key):  # Translated file from encrypted form
-        binn = ''
-        j = ''
-        for i in string:
-            j += i
-            if j in key:
-                binn += key[j]
-                j = ''
-        return binn
-    
+    def decode(string, key):
+        binary_string = ''
+        substring = ''
+        for sym in string:
+            substring += sym
+            if substring in key:
+                binary_string += key[substring]
+                substring = ''
+        return binary_string
+
+    """ Finding an almost decrypted string """
     @staticmethod
-    def expand(string, extra_char):  # Finding an almost decrypted string
+    def expand(string, extra_char):
         output_string = ''
-        for i in string:
-            sym = str(bin(ord(i))[2:])
+        for num in string:
+            sym = str(bin(ord(num))[2:])
             while len(sym) % 8 != 0:
                 sym = '0' + sym
             output_string += sym
         if extra_char != 0:
             output_string = output_string[:-extra_char]
         return output_string
-    
+
+    """ Reading encryption keys at the beginning of a file """
     @staticmethod
-    def search_keys(string, sim, delimiter):  # Reading encryption keys at the beginning of a file
-        b = ''
-        for i in string:
-            if i != sim:
-                b += i
-        s = b.split(delimiter)[:-1]
+    def search_keys(string, sim, delimiter):
+        header_string = ''
+        for sym in string:
+            if sym != sim:
+                header_string += sym
+        split_header_string = header_string.split(delimiter)[:-1]
         key = []
         values = []
-        for i in s:
-            key.append(i[0])
-            values.append(i[1:])
+        for sym in split_header_string:
+            key.append(sym[0])
+            values.append(sym[1:])
         keys_dict = dict(zip(values, key))
         return keys_dict
